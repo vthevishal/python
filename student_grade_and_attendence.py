05041afc15aaa188ee2103c1bc30cf74dc42d034
vthevishal <60169794+vthevishal@users.noreply.github.com>
2020-03-11 13:40:35 +0530

    ---Student grade and attendance generator--

---
+++ student_grade_and_attendence.py
@@ -0,0 +1,43 @@
+print("---Student grade and attendence generator---")
+z=input("enter your name\t ")
+while(True):
+    try:
+        a=int(input("enter grade1\t"))
+        break
+    except:
+        print("try again")
+while(True):
+    try:
+        b=int(input("enter grade2\t"))
+        break
+    except:
+        print("try again")
+while(True):
+    try:
+        c=int(input("enter total number of class\t"))
+        if(c!=0): #To avoid divide by zero error
+            break
+    except:
+        print("try again")
+while(True):
+    try:
+     d=int(input("enter total number of absent\t"))
+     break
+    except:
+        print("try again")
+
+
+f=((c-d)/c)*100                                          #avg_of_attendence
+e=(a+b)/2                                                #avg_of_grade
+print("Welcome {} Your Result Is".format(z))
+print('cummulative of grade is {}'.format(e))
+print("The attendance rate is ",(round(f,2)),"%" )
+if(f>=80):                                              #chk attendance is above 80%
+    if(e>=6):   #chk grade is above 6
+        print("You are  approved")
+    else:
+        print("You are Failed due to Grade")
+elif(e>=6):                                             #chk Failed only in attendance and not in Grade
+    print("You are Failed due to lack of attendance")
+else:
+    print("You Failed due to attendance and Grade")
